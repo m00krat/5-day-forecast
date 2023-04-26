367c9fbf779d4024db6e3bfc3b602541357c0c61
m00krat <prevostm17@yahoo.com>
2023-04-26 17:34:08 -0400

    final-version

--- js/script.js
+++ js/script.js
@@ -1,49 +1,98 @@
 // replace apiKey with your actual API key
 const apiKey = "3d164fa37879846ea87d0b9643681167";
 
-// function to get weather data for a city
-function getWeatherData(cityName) {
-  // use API to get the latitude and longitude for the city
-  const geoUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`;
-  fetch(geoUrl)
+function getWeatherForecast(cityName) {
+  // use API to get the 5-day forecast data for the city
+  const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=${apiKey}`;
+  fetch(forecastUrl)
     .then(response => response.json())
     .then(data => {
-      const { lat, lon } = data[0];
-      // use the latitude and longitude to get the current weather data for the city
-      const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=imperial&appid=${apiKey}`;
-      fetch(currentWeatherUrl)
-        .then(response => response.json())
-        .then(data => {
-          // display weather data on the page
-          const cityNameElement = document.getElementById("cityName");
-          const dateElement = document.getElementById("date");
-          const weatherIconElement = document.getElementById("weatherIcon");
-          const temperatureElement = document.getElementById("temperature");
-          const humidityElement = document.getElementById("humidity");
-          const windSpeedElement = document.getElementById("windSpeed");
+      // display forecast data on the page
+      const cityNameElement = document.getElementById("cityName");
+      const forecastListElement = document.getElementById("forecastList");
 
-          cityNameElement.textContent = cityName;
-          dateElement.textContent = moment(data.dt * 1000).format("MMM D, YYYY");
-          weatherIconElement.setAttribute("src", `https://openweathermap.org/img/w/${data.weather[0].icon}.png`);
-          temperatureElement.textContent = `${Math.round(data.main.temp)}°F`;
-          humidityElement.textContent = `${data.main.humidity}%`;
-          windSpeedElement.textContent = `${Math.round(data.wind.speed)} mph`;
-        })
-        .catch(error => console.error(error));
+      cityNameElement.textContent = cityName;
+      forecastListElement.innerHTML = "";
+
+      for (let i = 0; i < data.list.length; i += 8) {
+        const forecastData = data.list[i];
+        const date = moment(forecastData.dt * 1000).format("MMM D, YYYY");
+        const weatherIconUrl = `https://openweathermap.org/img/w/${forecastData.weather[0].icon}.png`;
+        const temperature = `${Math.round(forecastData.main.temp)}°F`;
+        const humidity = `${forecastData.main.humidity}% humidity`;
+        const windSpeed = `${Math.round(forecastData.wind.speed)} mph`;
+
+        const forecastItemElement = document.createElement("li");
+        forecastItemElement.innerHTML = `
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">${date}</h5>
+              <img src="${weatherIconUrl}" alt="Weather Icon">
+              <p class="card-text">${temperature}</p>
+              <p class="card-text">${humidity}</p>
+              <p class="card-text">${windSpeed}</p>
+            </div>
+          </div>
+        `;
+        forecastListElement.appendChild(forecastItemElement);
+      }
+
+      // save the city into local storage
+      const cityList = JSON.parse(localStorage.getItem("cityList")) || [];
+      if (!cityList.includes(cityName)) {
+        cityList.push(cityName);
+        localStorage.setItem("cityList", JSON.stringify(cityList));
+        renderCityButtons(cityList);
+      }
+      localStorage.setItem("lastSearchedCity", cityName);
+        const clearCitiesButton = document.getElementById("clearCitiesButton");
+        clearCitiesButton.addEventListener("click", () => {
+        localStorage.removeItem("cityList");
+        renderCityButtons([]);
+});
+
     })
     .catch(error => console.error(error));
 }
 
-// function to handle form submission
-function handleSubmit(event) {
-  event.preventDefault(); // prevent default form submission behavior
-  const cityInput = document.getElementById("cityInput");
-  const cityName = cityInput.value.trim(); // get city name from input
-  if (cityName === "") return; // if city name is empty, do nothing
-  cityInput.value = ""; // clear the input field
-  getWeatherData(cityName); // get weather data for the entered city
+// retrieve the last searched city from local storage
+const lastSearchedCity = localStorage.getItem("lastSearchedCity");
+
+// if the last searched city is not null, display it as a button or link
+if (lastSearchedCity !== null) {
+  getWeatherForecast(lastSearchedCity); // add this line to get the 5-day forecast for the last searched city
 }
 
-// add event listener for form submission
-const weatherForm = document.getElementById("weatherForm");
-weatherForm.addEventListener("submit", handleSubmit);
+function handleSubmit(event) {
+  event.preventDefault();
+  const cityInput = document.getElementById("cityInput");
+  const cityName = cityInput.value.trim();
+
+  if (cityName.length === 0) {
+    return;
+  }
+
+  getWeatherForecast(cityName);
+
+  cityInput.value = "";
+}
+
+function renderCityButtons(cityList) {
+  const cityListElement = document.getElementById("cityList");
+  cityListElement.innerHTML = "";
+  const form = document.getElementById("weatherForm");
+  form.addEventListener("submit", handleSubmit);
+
+
+  cityList.forEach(cityName => {
+    const cityButtonElement = document.createElement("button");
+    cityButtonElement.textContent = cityName;
+    cityButtonElement.classList.add("btn", "btn-secondary", "mx-1");
+    cityButtonElement.addEventListener("click", () => getWeatherForecast(cityName));
+    cityListElement.appendChild(cityButtonElement);
+  });
+}
+
+// retrieve the city list from local storage and render the buttons
+const cityList = JSON.parse(localStorage.getItem("cityList")) || [];
+renderCityButtons(cityList);
